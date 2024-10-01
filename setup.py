b1cbacf8ae416e6a7b1e51dc337935579ce29bee
Kelly Anderson <69013811+anderske-msu@users.noreply.github.com>
2024-10-01 16:57:54 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="SympNetTorch",
-    version="0.0.1",
+    version="0.0.2",
     author="Kelly Anderson",
     author_email="anderske@frib.msu.edu",
     description="Sympletic PyTorch Modules",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/anderske-msu/SympNetsTorch",
+    url="https://github.com/MSU-Beam-Dynamics/SympNetsTorch",
     project_urls={
-        "Bug Tracker": "https://github.com/anderske-msu/SympNetsTorch/issues"
+        "Bug Tracker": "https://github.com/MSU-Beam-Dynamics/SympNetsTorch/issues"
     },
     packages=["SympNetTorch"],
     install_requires=["torch"],
